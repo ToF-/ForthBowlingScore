e91df33714fff3395ea91d93363d7c03a17def68
Christophe Thibaut <cthibaut@octo.com>
2020-08-27 23:46:35 +0200

    refactoring

--- Bowling.fs
+++ Bowling.fs
@@ -5,10 +5,20 @@
     16 rshift dup 
     if 1- true then ;
 
+: set-last-throw ( pins -- value )
+    1+ 16 lshift ;
+
+: reset-last-throw ( -- value )
+    65535 and ;
+
 : last-throw! ( game,pins -- game )
     over last-throw 0= if 
-        1+ 16 lshift rot or 
-    else drop drop 65635 and then ;
+        set-last-throw 
+        rot or 
+    else 
+        drop drop 
+        reset-last-throw
+    then ;
 
 : knock-down ( game,pins -- game )
     dup -rot
