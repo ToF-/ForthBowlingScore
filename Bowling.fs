275e507ae2227ef032c9a34094683609510b9e34
Christophe Thibaut <cthibaut@octo.com>
2020-08-28 13:45:30 +0200

    add-throw increases the score

--- Bowling.fs
+++ Bowling.fs
@@ -3,4 +3,7 @@ variable score
 : start-game
     score off ;
 
+: add-throw ( pins -- ) 
+    score +! ;
+
 
--- TestBowling.fs
+++ TestBowling.fs
@@ -5,5 +5,11 @@ t{ ." start-game sets the score to zero " cr
     start-game 
     score @ 0 ?s
 }t
+
+t{ ." add-throw increases the score " cr
+    start-game
+    4 add-throw 
+    score @ 4 ?s
+}t
 cr .fut-tests-result
 bye
