13e91ffa4f7bc64c3f9cf378f9e6d90b09535fb8
Christophe Thibaut <cthibaut@octo.com>
2020-08-28 13:52:00 +0200

    after a spare, the next throw counts as bonus

--- Bowling.fs
+++ Bowling.fs
@@ -1,9 +1,15 @@
 variable score
+variable bonus
+variable last-throw
 
 : start-game
-    score off ;
+    score off 
+    bonus off ;
 
 : add-throw ( pins -- ) 
+    bonus @ if dup score +! then
+    dup last-throw @ + 10 = if bonus on then
+    dup last-throw !
     score +! ;
 
 
--- TestBowling.fs
+++ TestBowling.fs
@@ -11,5 +11,13 @@ t{ ." add-throw increases the score " cr
     4 add-throw 
     score @ 4 ?s
 }t
+
+t{ ." after a spare, the next throw counts as bonus " cr
+    start-game
+    4 add-throw
+    6 add-throw
+    3 add-throw
+    score @ 16 ?s 
+}t
 cr .fut-tests-result
 bye
