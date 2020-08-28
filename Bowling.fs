5bc26070993d5af91fd31fcaa8c30038b1b5e323
Christophe Thibaut <cthibaut@octo.com>
2020-08-28 13:53:55 +0200

    refactoring

--- Bowling.fs
+++ Bowling.fs
@@ -6,9 +6,14 @@ variable last-throw
     score off 
     bonus off ;
 
+: collect-bonus ( pins -- ) 
+    bonus @ if dup score +! then ;
+
+: calc-next-bonus ( pins -- )
+    dup last-throw @ + 10 = if bonus on then ;
 : add-throw ( pins -- ) 
-    bonus @ if dup score +! then
-    dup last-throw @ + 10 = if bonus on then
+    collect-bonus
+    calc-next-bonus
     dup last-throw !
     score +! ;
 
