2f6dc7351cfbb273d7d117f596c99f8635806d1a
Christophe Thibaut <cthibaut@octo.com>
2020-08-28 14:03:48 +0200

    refactoring

--- Bowling.fs
+++ Bowling.fs
@@ -11,17 +11,25 @@ variable half-frame
 : collect-bonus ( pins -- ) 
     bonus @ if dup score +! then ;
 
-: calc-next-bonus ( pins -- )
-    half-frame @ if
-        dup last-throw @ + 10 = if bonus on then 
-    then ;
+: spare? ( pins -- flag )
+    last-throw @ + 10 = ;
 
+: half-frame? ( -- flag )
+    half-frame @ ;
+
+: calc-next-bonus ( pins -- )
+    half-frame? 
+    over spare? and
+    if bonus on then ;
+
+: advance-frame 
+    half-frame? 0= half-frame ! ;
 
 : add-throw ( pins -- ) 
     collect-bonus
     calc-next-bonus
     dup last-throw !
     score +! 
-    half-frame @ 0= half-frame ! ;
+    advance-frame ;
 
 
