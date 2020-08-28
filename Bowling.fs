da176cb4eb777159b4e0f7c3fa17690ba5dade3a
Christophe Thibaut <cthibaut@octo.com>
2020-08-28 13:43:20 +0200

    start again

--- Bowling.fs
+++ Bowling.fs
@@ -1,43 +1,6 @@
-hex
-F000 constant last-throw-mask
-0FFF constant score-mask
-decimal
-12   constant last-throw-offset
-: start-game ( -- game )
-    0 ;
+variable score
 
-: reset ( value -- value )
-    -1 xor and ;
-
-: last-throw ( game -- pins,flag )
-    last-throw-offset rshift dup 
-    if 1- true then ;
-
-: set-last-throw ( game,pins -- game )
-    1+ last-throw-offset lshift or ;
-
-: reset-last-throw ( game -- game )
-    last-throw-mask reset ;
-
-: last-throw! ( game,pins -- game )
-    over last-throw 0= if 
-        set-last-throw 
-    else 
-        2drop
-        reset-last-throw
-    then ;
-
-: add-to-score! ( game,pins -- game )
-    + ;
-
-: knock-down ( game,pins -- game )
-    tuck
-    last-throw!
-    swap
-    add-to-score! ;
-
-: score ( game -- score ) 
-    score-mask and ;
-
+: start-game
+    score off ;
 
 
--- TestBowling.fs
+++ TestBowling.fs
@@ -3,27 +3,7 @@ require Bowling.fs
 
 t{ ." start-game sets the score to zero " cr
     start-game 
-    score 0 ?s
+    score @ 0 ?s
 }t
-t{ ." knock-down increases the score by the number of pins knocked down " cr
-    start-game 
-    4 knock-down
-    score 4 ?s
-}t
-t{ ." last-throw gives a false flag if we are at the beginning of a new frame " cr
-    start-game 
-    last-throw ?false 
-}t
-t{ ." last-throw gives the number of pins knocked down on last throw and a true flag if we are in the second half of a frame " cr
-    start-game
-    7 knock-down
-    last-throw ?true 7 ?s
-}t
-t{ ." after the second throw, last-throw gives a false flag " cr
-    start-game
-    3 knock-down
-    2 knock-down
-    last-throw ?false
-}t
-.fut-tests-result cr bye
-
+cr .fut-tests-result
+bye
