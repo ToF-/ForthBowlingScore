f10244c9dd9a9644fade6e29f3563969ab56b650
Christophe Thibaut <cthibaut@octo.com>
2020-08-29 06:45:20 +0200

    2. after adding a roll, score increases by the number of pins knocked down

--- TestBowling.fs
+++ TestBowling.fs
@@ -1,9 +1,20 @@
 require fut/fut.fs
 require Bowling.fs
 
-t{ ." at the start of a game, score is zero " cr
+t{ ." at the start of a game, score is zero" cr
     START-GAME  SCORE 0 ?S
 }t
 
+t{ ." after adding a roll, score increases by the number of pins knocked down" cr
+    START-GAME
+    4 ADD-ROLL
+    SCORE 4 ?S
+}t
+    START-GAME
+    4 ADD-ROLL
+    SCORE 4 ?S
+}t
+
+.fut-tests-result
 bye
 
