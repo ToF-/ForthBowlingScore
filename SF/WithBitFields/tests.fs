7964cc896d7fa83a59aad511bba8ff5fb14f82ba
Christophe Thibaut <cthibaut@octo.com>
2020-09-07 23:24:27 +0200

    changed from swiftforth to gforth

--- SF/WithBitFields/tests.fs
+++ SF/WithBitFields/tests.fs
@@ -1,7 +1,6 @@
 PAGE
-WARNING OFF
 
-INCLUDE %SwiftForth/unsupported/tests/ttester.fs
+INCLUDE ffl/tst.fs
 INCLUDE Bowling.fs
 
 BITSTRUCT FOO
@@ -10,29 +9,31 @@ BITSTRUCT FOO
 ENDBITSTRUCT
 : TESTS
 T{ ." A bitfield yields its offset and size" cr
-    BAR QUX -> 0 4 4 3
+    BAR 4 ?s 0 ?s 
+    QUX 3 ?s 4 ?s
 }T
 
 T{ ." mask sets n bits to 1 " cr
-    4 MASK 3 MASK -> 15 7
+    4 MASK 15 ?s
+    3 MASK  7 ?s
 }T 
 
 T{ ." offset moves a bit field value to its position " cr
-    3 4 OFFSET -> 48
+    3 4 OFFSET 48 ?s
 }T
 
 T{ ." %> extracts a bit field value given offset and size " cr
-    0013 1 3 %> -> 6
+    0013 1 3 %> 6 ?s
 }T
 
 T{ ." <% save a bit field value given offset and size " cr
-    32 6 1 3 <% -> 44
+    32 6 1 3 <% 44 ?s
 }T
 T{ ." a bitfield value can be saved and retrieved " cr
-    255 6 QUX <% 
-      10 BAR  <%
-     DUP QUX %>
-    SWAP BAR %> -> 6 10
+    255 6 QUX <%  
+      10 BAR  <% 
+     DUP QUX %> 6 ?s 
+      BAR %> 10 ?S
 }T
 ;
 
--- SF/WithVariables/tests.fs
+++ SF/WithVariables/tests.fs
@@ -1,29 +1,28 @@
 PAGE
-WARNING OFF
 
-INCLUDE %SwiftForth/unsupported/tests/ttester.fs
+INCLUDE ffl/tst.fs
 INCLUDE Bowling.fs
 
 : TESTS
 T{
    ." when starting a game, the score is zero " cr
-    START-GAME SCORE @ -> 0 
+    START-GAME SCORE @ 0 ?S 
 }T
 
 T{ ." after adding a roll, the score increases " cr
-    START-GAME 7 ADD-ROLL SCORE @ -> 7
+    START-GAME 7 ADD-ROLL SCORE @ 7 ?S
 }T
 
 T{ ." after a spare the next roll is counted as a bonus " cr
-    START-GAME 6 ADD-ROLL 4 ADD-ROLL 2 ADD-ROLL SCORE @ -> 14
+    START-GAME 6 ADD-ROLL 4 ADD-ROLL 2 ADD-ROLL SCORE @ 14 ?S
 }T
 
 T{ ." two rolls in different frames cannot make spare " cr
-    START-GAME 2 ADD-ROLL 4 ADD-ROLL 6 ADD-ROLL 3 ADD-ROLL SCORE @ -> 15
+    START-GAME 2 ADD-ROLL 4 ADD-ROLL 6 ADD-ROLL 3 ADD-ROLL SCORE @ 15 ?S
 }T
 
 T{ ." a strike at first rolls creates bonus for the next two rolls " cr
-    START-GAME 10 ADD-ROLL 4 ADD-ROLL 6 ADD-ROLL SCORE @ -> 30 
+    START-GAME 10 ADD-ROLL 4 ADD-ROLL 6 ADD-ROLL SCORE @ 30 ?S 
 }T
 
 T{ ." after ten frames, throws are not counted anymore " cr
@@ -32,7 +31,7 @@ T{ ." after ten frames, throws are not counted anymore " cr
     3 ADD-ROLL 5 ADD-ROLL 3 ADD-ROLL 5 ADD-ROLL 3 ADD-ROLL 5 ADD-ROLL
     3 ADD-ROLL 5 ADD-ROLL 3 ADD-ROLL 5 ADD-ROLL 3 ADD-ROLL 5 ADD-ROLL
     3 ADD-ROLL 5 ADD-ROLL 3 ADD-ROLL 5 ADD-ROLL 3 ADD-ROLL 5 ADD-ROLL
-    SCORE @ -> 80
+    SCORE @ 80 ?S
 }T
 
 T{ ." after ten frames, throws don't generate bonus anymore "
@@ -40,7 +39,7 @@ T{ ." after ten frames, throws don't generate bonus anymore "
     10 ADD-ROLL 10 ADD-ROLL 10 ADD-ROLL 10 ADD-ROLL 10 ADD-ROLL 
     10 ADD-ROLL 10 ADD-ROLL 10 ADD-ROLL 10 ADD-ROLL 10 ADD-ROLL 
     10 ADD-ROLL 10 ADD-ROLL 10 ADD-ROLL 10 ADD-ROLL 
-    SCORE @ -> 300
+    SCORE @ 300 ?S
 }T
 ;
 
