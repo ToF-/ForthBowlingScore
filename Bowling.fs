e0f823db47d81078dbd31942f0ae449f584c071d
Christophe Thibaut <cthibaut@octo.com>
2020-09-08 20:45:30 +0200

    small definitions, bottom up approach

--- Bowling.fs
+++ Bowling.fs
@@ -1,32 +1,52 @@
+HEX
 1 CONSTANT SPARE
 5 CONSTANT STRIKE
 
 : FRAME# ( frame -- n )
     4 RSHIFT ;
 
-: NEXT-FRAME ( frame -- frame' )
-    240 AND 16 + 160 MIN ;
+: FRAME++ ( frame -- frame' )
+    F0 AND 10 + A0 MIN ;
 
-: OPEN-FRAME? ( frame -- flag )
-    15 AND ;
+: OPEN? ( frame -- flag )
+    0F AND ;
 
 : ROLL>FRAME ( pins,frame -- frame' )
-    240 AND SWAP 1+ OR ;
+    F0 AND SWAP 1+ OR ;
 
 : FRAME>ROLL ( frame -- pins )
-    15 AND 1- ;
+    0F AND 1- ;
+
+: SPARE? ( pins,frame -- flag )
+    FRAME>ROLL + 0A = ;
+
+: STRIKE? ( pins,frame -- flag )
+    DROP 0A = ;
 
 : BONUS ( pins,frame -- bonus )
-    DUP OPEN-FRAME? IF
-        FRAME>ROLL + 10 = IF
-            SPARE
-        ELSE 
-            0
-        THEN
+    DUP OPEN? IF
+        SPARE? IF SPARE ELSE 0 THEN
     ELSE
-       DROP 10 = IF 
-           STRIKE 
-       ELSE 
-          0 
-       THEN
+        STRIKE? IF STRIKE ELSE 0 THEN
     THEN ;
+
+: FACTOR ( bonus -- factor )
+    3 AND ;
+
+: ADVANCE-BONUS ( bonus -- bonus' )
+    2 RSHIFT ;
+
+: FRAME>GAME ( frame,game -- game' )
+    FFF00 AND OR ;
+
+: GAME>FRAME ( game -- frame )
+    000FF AND ;
+    
+: BONUS>GAME ( bonus,game -- game' )
+    FF8FF AND SWAP 8 LSHIFT OR ;
+
+: GAME>BONUS ( game -- bonus )
+    00700 AND 8 RSHIFT ;
+
+
+DECIMAL
--- tests.fs
+++ tests.fs
@@ -1,21 +1,21 @@
 INCLUDE ffl/tst.fs
 INCLUDE Bowling.fs
 
-T{ ." next-frame increments frame count unless frame count is ten" cr
+T{ ." frame++ increments frame count unless frame count is ten" cr
     0 FRAME# 0 ?S
-    0 NEXT-FRAME FRAME# 1 ?S
-    0 NEXT-FRAME NEXT-FRAME NEXT-FRAME NEXT-FRAME NEXT-FRAME NEXT-FRAME
-      NEXT-FRAME NEXT-FRAME NEXT-FRAME NEXT-FRAME NEXT-FRAME NEXT-FRAME
+    0 FRAME++ FRAME# 1 ?S
+    0 FRAME++ FRAME++ FRAME++ FRAME++ FRAME++ FRAME++
+      FRAME++ FRAME++ FRAME++ FRAME++ FRAME++ FRAME++
     FRAME# 10 ?S
 }T
 
 T{ ." a frame is not open initially or if its count has just been incremented " cr
-    0 OPEN-FRAME? ?false
-    0 NEXT-FRAME OPEN-FRAME? ?false
+    0 OPEN? ?false
+    0 FRAME++ OPEN? ?false
 }T
 
 T{ ." a frame is open after saving the previous roll " cr
-    7 0 ROLL>FRAME OPEN-FRAME? ?true
+    7 0 ROLL>FRAME OPEN? ?true
 }T
 
 T{ ." if a frame is open it contains the previous roll " cr
@@ -34,5 +34,32 @@ T{ ." a complement to ten on an open frame gives a spare bonus " cr
     6 0 ROLL>FRAME
     4 SWAP  BONUS SPARE ?s
 }T
+
+T{ ." the bonus factor from a spare is 1 " cr
+    SPARE FACTOR 1 ?s
+}T
+
+T{ ." the bonus factor from a strike is 1 " cr
+    STRIKE FACTOR 1 ?s
+}T
+
+T{ ." the bonus factor from a strike on a strike is 2 " cr
+    STRIKE 1+ FACTOR 2 ?s
+}T
+
+T{ ." advance bonus switch the bonus to next throw " cr
+    SPARE  ADVANCE-BONUS 0 ?s
+    STRIKE ADVANCE-BONUS 1 ?s
+    STRIKE 1+ ADVANCE-BONUS 1 ?s
+}T
+
+T{ ." a game includes frame and bonus information " cr
+    7 0 ROLL>FRAME 0 FRAME>GAME 
+    GAME>FRAME FRAME>ROLL 7 ?s
+
+    STRIKE 0 BONUS>GAME 
+    GAME>BONUS STRIKE ?s
+}T
+    
 BYE
 
