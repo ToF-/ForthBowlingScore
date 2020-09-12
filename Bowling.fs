e671ed66a4478f5b35badb2d53cf35da24f7590a
Christophe Thibaut <cthibaut@octo.com>
2020-09-12 12:22:27 +0200

    revised

--- Bowling.fs
+++ Bowling.fs
@@ -1,51 +1,70 @@
+\ Bowling.fs          gforth Bowling.fs <input.dat
 VARIABLE SCORE
 VARIABLE BONUS
-VARIABLE FRAME-STATE
 VARIABLE FRAME#
+VARIABLE FRAME
 
 : START
-    0 SCORE ! 0 BONUS ! 0 FRAME-STATE ! 0 FRAME# ! ;
+    0 SCORE !
+    0 BONUS !
+    0 FRAME# !
+    0 FRAME ! ;
 
-: SPARE! 1 BONUS ! ;
+: SPARE \ set the bonus to 1, no supplement
+    1 BONUS ! ;
 
-: STRIKE! BONUS @ 1+ 4 OR BONUS ! ;
+: STRIKE \ increase bonus, and set supplement to 1
+    BONUS @ 1+ 4 OR BONUS ! ;
 
-: BONUS> ( -- factor )
+: BONUS> ( -- n ) \ produce bonus factor, get supplement ready
     BONUS @ DUP 3 AND
-    SWAP 2 RSHIFT BONUS ! ;
+    SWAP 2/ 2/ BONUS ! ;
 
-: FRAME>
+: COLLECT-BONUS ( #pins -- ) \ add extra points to score and advance bonus
+    BONUS> * SCORE +! ;
+
+: FRAME#> \ advance frame count
     FRAME# @ 1+ 10 MIN FRAME# ! ;
 
-: COLLECT-BONUS ( #pins -- ) BONUS> * SCORE +! ;
+: OPEN-FRAME? ( -- ? )
+    FRAME @ ;
 
-: OPEN-FRAME? ( -- flag ) FRAME-STATE @ ;
+: CLOSE-FRAME \ close frame and increment frame count
+    0 FRAME ! FRAME#> ;
 
-: NEW-FRAME? ( -- flag ) OPEN-FRAME? 0= ;
+: NEW-FRAME? ( -- ? )
+    OPEN-FRAME? 0= ;
 
-: CLOSE-FRAME!  0 FRAME-STATE ! FRAME> ;
+: OPEN-FRAME ( #pins -- ) \ open frame, saving 1st roll value+1
+    1+ FRAME ! ;
 
-: OPEN-FRAME! ( #pins -- ) 1+ FRAME-STATE ! ;
-
-: LAST-ROLL ( -- #pins ) FRAME-STATE @ 1- ;
+: LAST-ROLL ( -- #pins ) \ retrieve the first roll value
+    FRAME @ 1- ;
 
 : CHECK-STRIKE ( #pins -- )
-    DUP 10 = IF STRIKE! CLOSE-FRAME! ELSE OPEN-FRAME! THEN ;
+    DUP 10 = IF
+        DROP STRIKE CLOSE-FRAME
+    ELSE
+        OPEN-FRAME
+    THEN ;
 
 : CHECK-SPARE ( #pins -- )
-    LAST-ROLL + 10 = IF SPARE! THEN CLOSE-FRAME! ;
+    LAST-ROLL + 10 = IF SPARE THEN
+    CLOSE-FRAME ;
 
 : CHECK-BONUS ( #pins -- )
     NEW-FRAME? IF CHECK-STRIKE ELSE CHECK-SPARE THEN ;
 
 : ROLL+ ( #pins -- )
     DUP COLLECT-BONUS
-    FRAME# @ 0 10 WITHIN IF 
-        DUP SCORE +!  
-        CHECK-BONUS 
+    FRAME# @ 0 10 WITHIN IF
+        DUP SCORE +!
+        CHECK-BONUS
+    ELSE
+        DROP
     THEN ;
 
-: SKIP-NON-DIGIT ( -- d )
+: SKIP-NON-DIGIT ( -- n )
     BEGIN KEY DIGIT? 0= WHILE REPEAT ;
 
 : GET-NUMBER ( -- n )
@@ -56,9 +75,9 @@ VARIABLE FRAME#
     0= UNTIL ;
 
 : BOWLING
-    GET-NUMBER ?DUP 0 DO
+    GET-NUMBER 0 DO
         START
-        GET-NUMBER ?DUP 0 DO
+        GET-NUMBER 0 DO
             GET-NUMBER ROLL+
         LOOP
         SCORE ? CR
